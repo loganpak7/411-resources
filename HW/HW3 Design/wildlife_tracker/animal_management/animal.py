5d8dcd8fe68d2f50bba1b3da389f97346d0cd611
Logan Pak <loganpak@bu.edu>
2024-10-07 20:15:33 -0400

    Added the wildlife tracker solution

--- HW/HW3 Design/wildlife_tracker/animal_management/animal.py
+++ HW/HW3 Design/wildlife_tracker/animal_management/animal.py
@@ -1,5 +1,5 @@
 from typing import Any, Optional
 
 class Animal:
-
+       
     pass
--- HW/HW3 Design/wildlife_tracker/animal_management/animal_manager.py
+++ HW/HW3 Design/wildlife_tracker/animal_management/animal_manager.py
@@ -1,6 +1,5 @@
 from typing import Optional
-
-from wildlife_tracker.animal_managment.animal import Animal
+from wildlife_tracker.animal_management.animal import Animal
 
 class AnimalManager:
 
@@ -8,10 +7,12 @@ class AnimalManager:
         animals: dict[int, Animal] = {}
 
     def get_animal_by_id(self, animal_id: int) -> Optional[Animal]:
-        pass
+        return self.animals.get(animal_id)
 
-    def register_animal(Animal) -> None:
-        pass
+    def register_animal(self, Animal) -> None:
+        animal_id = len(self.animals) + 1
+        self.animals[animal_id] = animal
 
-    def remove_animal(animal_id: int) -> None:
-        pass
+    def remove_animal(self, animal_id: int) -> None:
+        if(animal_id in self.animals):
+            del self.animals[animal_id]
